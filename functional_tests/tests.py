f53ce3d76e75eccd8f806ce41bdb0089dcbac03b
hstrey <Helmut.Strey@gmail.com>
2016-04-01 14:43:49 -0400

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,7 +40,7 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/list/.+')
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is still a text box inviting her to add another item.  She
@@ -73,7 +73,7 @@ class NewVisitorTest(LiveServerTestCase):
         
         # Francis gets his own url
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
         
         # again, there is no trace of Edith's list
